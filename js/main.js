affed5e485f79948e1f8cc45cb3d298df8d8f50a
fgt.fernando@gmail.com <fgt.fernando@gmail.com>
2022-11-19 21:52:34 -0300

    chore: update javascript main files

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Oracle APEX Plugin
  * author: Fernando D. Richter
  * version: v1.0.0
- * uppy version: v3.3.0
+ * uppy version: v3.3.1
  * license: MIT
  */
 
@@ -10,63 +10,63 @@ const component = {
   init: function () {
     // get plugin object
     const action = this.action
-    // get pluigin ajax identifier
-    const ajaxIdentifier = action.ajaxIdentifier
     // get plugin attributes
-    const restrictions        = JSON.parse(action.attribute01)
-    const dashboardOptions    = JSON.parse(action.attribute02)
-    const imageEditor         = (action.attribute03 === 'Y')
-    const targetElement       = action.attribute04
-    const additionalFeatures  = action.attribute05 || 'empty'
-    const usePresignUrl       = (action.attribute07 === 'Y')
-    const theme               = action.attribute08
-    const onCompleteCallback  = action.attribute09
-    const localePack          = action.attribute10
-    const destination         = action.attribute11
-    const uppyId              = action.attribute12 || Date.now()
-    const uploadLinkObjectKey = action.attribute13
+    const plugin = {
+      ajaxIdentifier: action.ajaxIdentifier,
+      restrictions: JSON.parse(action.attribute01),
+      dashboardOptions: JSON.parse(action.attribute02),
+      imageEditor: (action.attribute03 === 'Y'),
+      targetElement: action.attribute04,
+      additionalFeatures: action.attribute05 || 'empty',
+      usePresignUrl: (action.attribute07 === 'Y'),
+      theme: action.attribute08,
+      onCompleteCallback: action.attribute09,
+      localePack: action.attribute10,
+      destination: action.attribute11,
+      uppyId: action.attribute12 || Date.now(),
+      uploadLinkObjectKey: action.attribute13,
+    }
     // uppy code
     const uppy = new Uppy.Uppy({
       autoProceed: false,
-      locale: Uppy.locales[localePack],
-      id: uppyId,
-      restrictions,
+      locale: Uppy.locales[plugin.localePack],
+      id: plugin.uppyId,
+      restrictions: plugin.restrictions,
     })
     
     uppy.use(Uppy.Dashboard, {
       proudlyDisplayPoweredByUppy: false,
-      target: targetElement,
+      target: plugin.targetElement,
       allowMultipleUploads: true,
       inline: true,
       replaceTargetContent: true,
       showProgressDetails: true,
-      theme,
+      theme: plugin.theme,
       browserBackButtonClose: false,
       hideUploadButton: false,
       hideProgressAfterFinish: true,
-      ...dashboardOptions,
+      ...plugin.dashboardOptions,
     })
 
-    imageEditor && 
+    plugin.imageEditor && 
     uppy.use(Uppy.ImageEditor, { target: Uppy.Dashboard })
     
-    additionalFeatures.search('AUDIO') >= 0 &&
+    plugin.additionalFeatures.search('AUDIO') >= 0 &&
     uppy.use(Uppy.Audio, {target: Uppy.Dashboard, showAudioSourceDropdown: true})
     
-    additionalFeatures.search('WEBCAM') >= 0 &&
+    plugin.additionalFeatures.search('WEBCAM') >= 0 &&
     uppy.use(Uppy.Webcam, {target: Uppy.Dashboard, showVideoSourceDropdown: true})
 
-    additionalFeatures.search('SCREEN_CAPTURE') >= 0 &&
+    plugin.additionalFeatures.search('SCREEN_CAPTURE') >= 0 &&
     uppy.use(Uppy.ScreenCapture, { target: Uppy.Dashboard })
 
-    destination === 'AwsS3' &&
-    uppy.use(Uppy[destination], {
+    plugin.destination === 'AwsS3' &&
+    uppy.use(Uppy[plugin.destination], {
       async getUploadParameters (file) {
         let uploadParameters
-
-        if (usePresignUrl) {
+        if (plugin.usePresignUrl) {
           await apex.server.plugin(
-            ajaxIdentifier,
+            plugin.ajaxIdentifier,
             {
               x01: file.name,
               x02: file.type,
@@ -78,18 +78,16 @@ const component = {
             }
           )
         }
-
         return {
           method: 'put',
-          url: uploadParameters[uploadLinkObjectKey],
+          url: uploadParameters[plugin.uploadLinkObjectKey],
           fields: uploadParameters,
           headers: {"Content-Type": `${file.type}; charset=utf-8`},
         }
       }
     })
 
-    uppy.on('complete', eval(onCompleteCallback))
-
+    uppy.on('complete', eval(plugin.onCompleteCallback))
     return uppy
   }
 }
